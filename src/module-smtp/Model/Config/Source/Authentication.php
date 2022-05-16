6bcb184814d29799bc26ffceb8f6ebf2c87be17b
dholi <comunidade@dholi.dev>
2022-05-16 17:08:33 -0300

    Magento 2.4.4 support

--- src/module-smtp/Model/Config/Source/Authentication.php
+++ src/module-smtp/Model/Config/Source/Authentication.php
@@ -3,10 +3,10 @@
 * 
 * SMTP para Magento 2
 * 
-* @category     Ã©lOOm
+* @category     elOOm
 * @package      Modulo SMTP
-* @copyright    Copyright (c) 2021 Ã©lOOm (https://eloom.tech)
-* @version      1.0.0
+* @copyright    Copyright (c) 2022 elOOm (https://eloom.tech)
+* @version      2.0.0
 * @license      https://opensource.org/licenses/OSL-3.0
 * @license      https://opensource.org/licenses/AFL-3.0
 *
